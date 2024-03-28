60e18087f1c333a8d47d3d62c2aa8af6e7be18b9
Anand Singavarapu <doitwithpassionrnotatall@gmail.com>
2024-03-28 17:43:50 +0530

    Add counter folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // componentDidMount and componentDidUpdate combined
+    console.log("Component mounted or updated");
+    return () => {
+      // componentWillUnmount
+      console.log("Component will unmount");
+    };
+  }, [count]); // Only re-run the effect if count changes
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => (count ? setCount(count - 1) : count)}>
+        decrement
+      </button>
     </div>
   );
 }
 
-export default App;
+export default MyComponent;
