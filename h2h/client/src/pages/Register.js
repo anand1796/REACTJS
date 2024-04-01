233854802c182ac37ef664ff4c293f90df0a4d6b
Anand Singavarapu <doitwithpassionrnotatall@gmail.com>
2024-04-02 02:41:12 +0530

    Register Styled

--- h2h/client/src/pages/Register.js
+++ h2h/client/src/pages/Register.js
@@ -1,7 +1,61 @@
 import React from "react";
+import RegisterContainer from "./Register.styled";
+import { Link } from "react-router-dom";
+import Login from "./Login";
+import Logo from "../assets/h-2-h.png";
 
 const Register = () => {
-  return <div>register</div>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("form");
+  };
+
+  const handleChange = ("onclick", () => {});
+
+  return (
+    <>
+      <RegisterContainer>
+        <form
+          onSubmit={(event) => {
+            handleSubmit(event);
+          }}
+        >
+          <div className="product">
+            <img src={Logo} alt="Logo" />
+            <h1>heart2heart</h1>
+          </div>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter your username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Type your password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            name="confirm-password"
+            placeholder="Confirm your password"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Register</button>
+          <span>
+            <Link to={Login}>Already have an account? Login</Link>
+          </span>
+        </form>
+      </RegisterContainer>
+    </>
+  );
 };
 
 export default Register;
---
+++ h2h/client/src/pages/Register.styled.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+const RegisterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  background-color: #b3003c;
+  .product {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      height: 70px;
+    }
+    h1 {
+      font-family: "merienda";
+      color: white;
+    }
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    background-color: #c7738f;
+    padding: 20px 50px;
+    border-radius: 10px;
+    input {
+      background-color: transparent;
+      border: 1px solid #fd1a56;
+      border-radius: 10px;
+      padding: 10px 5px;
+      outline: none;
+      color: white;
+      &::placeholder {
+        color: white;
+      }
+    }
+    button {
+      background-color: #fd1a56;
+      padding: 10px 5px;
+      border-radius: 10px;
+      border: none;
+      cursor: pointer;
+    }
+    span {
+      border: 1px solid #fd1a56;
+      padding: 10px 5px;
+      border-radius: 10px;
+      a {
+        text-decoration: none;
+        color: white;
+      }
+    }
+  }
+`;
+
+export default RegisterContainer;
